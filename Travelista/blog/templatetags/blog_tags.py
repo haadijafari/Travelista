56a7fbd2c379f30aac9b852dd75af0ddc97f154b
Hadi Jafari <haadijafari2003@gmail.com>
2023-08-13 22:06:14 +0330

    Published Date
    [+] now posts care about published date and time
    [+] counted views now works

--- Travelista/blog/templatetags/blog_tags.py
+++ Travelista/blog/templatetags/blog_tags.py
@@ -1,18 +1,29 @@
 from django import template
 from blog.models import Post, Category
+from datetime import datetime   #This is for filtering by published_date
+
 
 register = template.Library()
 
 @register.inclusion_tag('blog/blog-popular-post.html')
 def latest_posts(arg=3):
-    posts = Post.objects.filter(status=1).order_by('-published_date')[:arg]
+    current_datetime = datetime.now()    #This is for filtering by published_date
+    posts = Post.objects.filter(published_date__lt=current_datetime)     #This is for filtering by published_date
+    posts = posts.filter(status=1).order_by('-published_date')[:arg]
     return {'posts': posts}
 
 @register.inclusion_tag('blog/blog-post-category.html')
 def categories():
-    posts = Post.objects.filter(status=1)
+    current_datetime = datetime.now()    #This is for filtering by published_date
+    posts = Post.objects.filter(published_date__lt=current_datetime)     #This is for filtering by published_date
+    posts = posts.filter(status=1)
     categories = Category.objects.all()
     cat_dict = {}
     for cat in categories:
         cat_dict[cat] = posts.filter(category=cat).count()
     return {'categories': cat_dict}
+
+
+@register.filter
+def get_by_id(lst, post_id):
+    return next((post for post in lst if post.id == post_id), None)
--- Travelista/blog/views.py
+++ Travelista/blog/views.py
@@ -2,15 +2,19 @@ from django.shortcuts import render, get_object_or_404
 from blog.models import Post
 from itertools import chain
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from datetime import datetime   #This is for filtering by published_date
+
 
 def blog_home(request, **kwargs):
-    posts = Post.objects.filter(status=1).order_by('-published_date')
+    current_datetime = datetime.now()    #This is for filtering by published_date
+    posts = Post.objects.filter(published_date__lt=current_datetime)     #This is for filtering by published_date
+    posts = posts.filter(status=1).order_by('-published_date')
     if cat_name := kwargs.get('cat_name'):
         posts = posts.filter(category__name=cat_name)
     if author := kwargs.get('author'):
         posts = posts.filter(author__username=author)
-    
-    posts = Paginator(posts,5)
+
+    posts = Paginator(posts, 5)
     try:
         page_number = request.GET.get('page')
         posts = posts.get_page(page_number)
@@ -22,20 +26,24 @@ def blog_home(request, **kwargs):
     content = {'posts': posts}
     return render(request, 'blog/blog-home.html', content)
 
+
 def blog_single(request, **kwargs):
     posts = Post.objects.filter(status=1)
     if pid := kwargs.get('pid'):
-        # post = posts.get(id=pid)
-        post = get_object_or_404(posts, pk=pid)
-    content = {'post': post}
+        post = posts.get(id=pid)
+        post.counted_views += 1
+        post.save()
+        # post = get_object_or_404(posts, pk=pid)
+    content = {'post': post, 'all_posts': posts }
     return render(request, 'blog/blog-single.html', content)
+
 
 def blog_search(request):
     posts = Post.objects.filter(status=1)
     if request.method == 'GET':
         if s := request.GET.get('search'):
             posts = posts.filter(content__icontains=s)
-            posts = chain(Post.objects.filter(title__icontains=s),posts)
+            posts = chain(Post.objects.filter(title__icontains=s), posts)
 
     # posts = Paginator(posts,3)
     # try:
@@ -46,5 +54,5 @@ def blog_search(request):
     # except EmptyPage:
     #     posts = posts.get_page(1)
 
-    context = {'posts':posts}
-    return render(request,'blog/blog-home.html',context)
+    context = {'posts': posts}
+    return render(request, 'blog/blog-home.html', context)
