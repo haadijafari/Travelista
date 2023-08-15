30eae231d519b6cdd1f995a12a8b9b5a15320f14
Hadi Jafari <haadijafari2003@gmail.com>
2023-08-15 10:54:32 +0330

    Contact Form
    [+] made a forms.py for Contact Model Form
    [+] Templates has been configured for popup message

---
+++ Travelista/mainPage/forms.py
@@ -0,0 +1,23 @@
+from django import forms
+from mainPage.models import Contact
+
+class ContactForm(forms.ModelForm):            
+    class Meta:
+        model = Contact
+        fields = "__all__"
+
+    def save(self, commit=True):
+        instance = super(ContactForm, self).save(commit=False)
+        
+        # Modify the 'name' field to 'Anonymous'
+        instance.name = 'Anonymous'
+        
+        if commit:
+            instance.save()
+        return instance
+    
+    def __init__(self, *args, **kwargs):
+        super(ContactForm, self).__init__(*args, **kwargs)
+        
+        # Set a default value for the 'subject' field (in this case blank)
+        self.fields['subject'].initial = ''
--- Travelista/mainPage/views.py
+++ Travelista/mainPage/views.py
@@ -1,10 +1,22 @@
 from django.shortcuts import render
+from mainPage.forms import ContactForm
+from django.contrib import messages
 
 def mainPage(request):
     return render(request, 'Travelista/index.html')
 
 def contact(request):
-    return render(request, 'Travelista/contact.html')
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.add_message(request, messages.SUCCESS,
+                                 'Your ticket submitted successfully')
+        else:
+            messages.add_message(request, messages.ERROR,
+                                 'Your ticket didn\'t submitted')
+    form = ContactForm()
+    return render(request, 'Travelista/contact.html', {'form': form})
 
 def about(request):
     return render(request, 'Travelista/about.html')
