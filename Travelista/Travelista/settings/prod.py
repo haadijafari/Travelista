422a5a73f3a673596fa4efb6cfa14a9caff7a462
Hadi Jafari <haadijafari2003@gmail.com>
2024-04-03 18:38:58 +0330

    [FIX] Media Root

--- Travelista/Travelista/settings/prod.py
+++ Travelista/Travelista/settings/prod.py
@@ -24,7 +24,7 @@ DATABASES = {
 STATIC_ROOT = '/home/haadija1/public_html/Travelista/static'
 MEDIA_ROOT = '/home/haadija1/public_html/Travelista/media'
 STATIC_URL = 'Travelista/static/'
-MEDIA_URL = 'Travelista/media/'
+MEDIA_URL = '/media/'
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
